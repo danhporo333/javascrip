ad5f404ea0f98eca2c3b44f2aea64f740ad3e1eb
duongdanh <129704902+duongdanh@users.noreply.github.com>
2023-11-17 14:36:51 +0700

    file

---
+++ bóng đèn/scrip.js
@@ -0,0 +1,17 @@
+var isOnlight = false;
+
+        function turnOnlight() {
+            document.getElementById('myImage').src = "assent/pic_bulbon.webp"
+            isOnlight = true;
+        }
+
+        function turnffOlight() {
+            document.getElementById('myImage').src = 'assent/pic_bulboff.gif'
+            isOnlight = false;
+        }
+
+
+// const number = 1;   // Khai báo đầu tiên
+// number = 2;   // gán giá trị
+
+// console.log(number); 
